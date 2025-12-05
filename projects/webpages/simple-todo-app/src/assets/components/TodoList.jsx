6eaece9b40b9914492313e4b514ca2a505666673
imswpotato <catnguyen.tgcn@gmail.com>
2025-12-04 21:06:29 -0600

    updated TodoList.jsx

--- projects/webpages/simple-todo-app/src/assets/components/TodoList.jsx
+++ projects/webpages/simple-todo-app/src/assets/components/TodoList.jsx
@@ -2,23 +2,17 @@ import '../styles/TodoList.css';
 
 // Add a TodoList component that displays a list of todo items
 export function TodoList(props) {
-    // Loop through the todos and create list items
-    const items = [];
-    for (let i = 0; i < props.todos.length; i++) {
-        items.push(
-            <li key={i}>
-            <input 
-                type="checkbox"
-                checked={props.todos[i].completed}/>
-            <span>{props.todos[i].text}</span>
-            </li>
-        )
-        
-    // Return the list of items and a button to remove completed items
     return (<>
-    <ul>
-        {items}
-    </ul>
-    <button>Remove Completed</button>
+        <ul>
+            {props.todos.map(todo => (
+                <li key={todo.id}>
+                    <input
+                        type="checkbox"
+                        checked={todo.completed} />
+                    {todo.text}
+                </li>
+            ))}
+        </ul>
+        <button>Remove Completed</button>
     </>)
-}}
+}
