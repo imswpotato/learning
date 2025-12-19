14b4dba2774949dcf2efb1133c2dac1d999f6c66
imswpotato <catnguyen.tgcn@gmail.com>
2025-12-19 12:12:04 -0600

    added MealCard.jsx

---
+++ projects/webpages/find-me-something-to-cook-v2/src/components/MealCard.jsx
@@ -0,0 +1,24 @@
+function MealCard({ recipe, onSave }) {
+    return (
+        <div className="recipe-card">
+            <h3>{recipe.strMeal}</h3>
+            <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+            <button onClick={() => onSave(recipe)}>Save to Notebook</button>
+            <a 
+            href={`https://www.themealdb.com/meal.php?c=${recipe.idMeal}`}
+            target="_blank"
+            className="recipe-link"
+            >
+                View Recipe
+            </a>
+
+            {onSave && (
+                <button className="save-recipe-button" onClick={() => onSave(recipe)}>
+                    Save to Notebook
+                </button>
+            )}
+            </div>
+    );
+}
+
+export default MealCard;
